411f27bab67281245542b067fb7630beeed90c4b
Hideyuki Aoshika <h-aoshika@sungrove.co.jp>
2026-05-06 20:54:43 +0900

    add: Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from 'react-ga4';
 import Header from './components/Header'
 import ActivityList from './pages/ActivityList'
 import ActivityDetail from "./pages/ActivityDetail";
 import Dashboard from "./pages/Dashboard"
 
+function PageTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: 'Pageview', page: location.pathname });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Header title='strava app router' />
+      <PageTracker />
       <Routes>
         <Route path="/" element={<ActivityList />} />
         <Route path="/activity/:id" element={<ActivityDetail />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4';
 import './index.css'
 import App from './App.tsx'
+
+ReactGA.initialize('G-DSTK2ZX0LE');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
